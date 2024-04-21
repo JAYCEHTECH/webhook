e75df3f6f4a615eef141965c3c81d341d57d78e1
mikegyamfi <codeage20@gmail.com>
2024-04-21 21:44:49 +0000

    hi

--- business_api/admin.py
+++ business_api/admin.py
@@ -4,12 +4,12 @@ from business_api import models
 from import_export.admin import ExportActionMixin
 from rest_framework.authtoken.models import Token
 
+
 # Register your models here.
 
 class CashBackAdmin(ExportActionMixin, admin.ModelAdmin):
-    list_display = ['user_id', 'user_number', 'amount']
+    list_display = ['reference', 'transaction_type']
 
 
 admin.site.register(models.CustomUser)
-admin.site.register(models.CashBack, CashBackAdmin)
-
+admin.site.register(models.Transaction, CashBackAdmin)
